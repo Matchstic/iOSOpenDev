b13eb91c65a9b436308259cb85161f4227c3422d
Matt Clarke <mclarke0805@gmail.com>
2019-07-14 20:24:31 +0100

    Add required templates for Xcode to pick up on

--- install.sh
+++ install.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# Check for root access
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exec sudo "$0" "$@"
-    exit 1
-fi
-
 # Define functions
 function patchXcode {
     BASE_PATH=$1
+    CURRENT_USER="$USER"
     echo "[*] (Re-)patching Xcode"
 
     # TODO: Update templates with current BASE_PATH instead of /opt/iOSOpenDev
 
     echo "[*] Linking specifications if necessary..."
     if [ ! -L "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Specifications" ]; then
-        ln -s "${BASE_PATH}/specifications/iPhoneOS/Specifications" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/"
-        ln -s "${BASE_PATH}/specifications/iPhoneSimulator/Specifications" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Xcode/"
+        sudo ln -s "${BASE_PATH}/specifications/iPhoneOS/Specifications" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/"
+        sudo ln -s "${BASE_PATH}/specifications/iPhoneSimulator/Specifications" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Xcode/"
     fi
+    
+    echo "[*] Transferring templates..."
+    if [ ! -e "/Users/${CURRENT_USER}/Library/Developer/Xcode/Templates/" ]; then
+        mkdir -p "/Users/${CURRENT_USER}/Library/Developer/Xcode/Templates/"
+    fi
+    cp -R "${BASE_PATH}/templates/" "/Users/${CURRENT_USER}/Library/Developer/Xcode/Templates/"
 
     echo "[*] Transferring SDKs..."
-    cp -R "${BASE_PATH}/deps/sdks/" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/"
+    sudo cp -R "${BASE_PATH}/deps/sdks/" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/"
 
     echo "[*] Patching latest SDK..."
-    /usr/libexec/PlistBuddy -c "Set DefaultProperties:AD_HOC_CODE_SIGNING_ALLOWED YES" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
-    /usr/libexec/PlistBuddy -c "Set DefaultProperties:CODE_SIGNING_REQUIRED NO" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
-    /usr/libexec/PlistBuddy -c "Set DefaultProperties:AD_HOC_CODE_SIGNING_ALLOWED YES" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/SDKSettings.plist
-    /usr/libexec/PlistBuddy -c "Set DefaultProperties:CODE_SIGNING_REQUIRED NO" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/SDKSettings.plist
+    sudo /usr/libexec/PlistBuddy -c "Set DefaultProperties:AD_HOC_CODE_SIGNING_ALLOWED YES" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
+    sudo /usr/libexec/PlistBuddy -c "Set DefaultProperties:CODE_SIGNING_REQUIRED NO" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
+    sudo /usr/libexec/PlistBuddy -c "Set DefaultProperties:AD_HOC_CODE_SIGNING_ALLOWED YES" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/SDKSettings.plist
+    sudo /usr/libexec/PlistBuddy -c "Set DefaultProperties:CODE_SIGNING_REQUIRED NO" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/SDKSettings.plist
 
     echo "[*] Patching minimum SDK version..."
-    /usr/libexec/PlistBuddy -c "Set MinimumSDKVersion 8.4" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Info.plist
-    /usr/libexec/PlistBuddy -c "Set MinimumSDKVersion 8.4" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Info.plist
+    sudo /usr/libexec/PlistBuddy -c "Set MinimumSDKVersion 8.4" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Info.plist
+    sudo /usr/libexec/PlistBuddy -c "Set MinimumSDKVersion 8.4" /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Info.plist
 
     echo "[!] Done patching Xcode"
 }
@@ -55,7 +55,7 @@ function installSimject {
     git submodule init 1>/dev/null && git submodule update 1>/dev/null 
     
     make setup 1>/dev/null
-    cp "${BASE_PATH}/deps/simject/bin/resim" /usr/local/bin/
+    sudo cp "${BASE_PATH}/deps/simject/bin/resim" /usr/local/bin/
     
     echo "[!] Installed simject"
     echo "[*] Added binary: resim"
